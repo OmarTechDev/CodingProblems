92b111609b4624f9342fca64b3e0dfe80a0151a4
Omar Oporto Bernal <omar.oportober@gmail.com>
2024-01-03 11:41:24 -0400

    command line card for rover added

--- roversGame/src/components/NavBar.tsx
+++ roversGame/src/components/NavBar.tsx
@@ -1,30 +1,6 @@
 import Logo from '../assets/logo.svg?react'
 import './Navbar.css'
 
-const ElementsBar = () => (
-  <div className="navbar-nav me-auto">
-    <ul className='navbar ul'>
-      <li className="nav-item">
-        <a href="/" className="nav-link elements">Home</a>
-      </li>
-      <li className="nav-item">
-        <a href="/about" className="nav-link elements">About</a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Projects
-        </a>
-        <ul className="dropdown-menu">
-          <li><a className="dropdown-item" href="/calculator">Calculator</a></li>
-          <li><a className="dropdown-item" href="/Multi">MultiApp</a></li>
-          <li><hr className="dropdown-divider" /></li>
-          <li><a className="dropdown-item" href="#action5">Coming soon</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
-)
-
 function Maintop() {
   return (
     <>
@@ -35,7 +11,6 @@ function Maintop() {
         <div className="navbar-brand">
           Rovers - Game
         </div>
-        <ElementsBar/>
       </nav>
     </>
   )
--- roversGame/src/components/Principal/Body.tsx
+++ roversGame/src/components/Principal/Body.tsx
@@ -1,40 +1,95 @@
 import { useState, ChangeEvent } from 'react';
 import './Body.css'
 import land from '../../assets/marsLand.jpg'
+import rover from '../../assets/roversFunctional.jpg'
 
 interface BoardProps {
   width: number;
   height: number;
 }
 
+const ComandLine: React.FC<BoardProps> = ({ width, height }) => {
+  const name = 'Ax-001'
+  return(
+    <div className="cardA text-bg-dark mb-3">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={rover} className="img-fluid rounded-start" alt="roverRepresent"/>
+        </div>
+        <div className="col-md-8">
+          <div className="card-bodyA">
+            <h5 className="card-title">Rovers '{name}' Command Line</h5><br/>
+            <div className="mb-3">
+              <label htmlFor="formGroupExampleInput" className="form-label">Initial Position:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formGroupExampleInput"
+                placeholder={`Example: 5 5 N  ||  Max ${width - 1} , ${height - 1}`}
+              /><br/>
+              <label htmlFor="formGroupExampleInput" className="form-label">Comand Line Input:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formGroupExampleInput"
+                placeholder={`Example: LMLMLMLMM`}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const Board: React.FC<BoardProps> = ({ width, height }) => {
-    const maxWidth = 700;
-    const maxHeight = 500;
+  const maxWidth = 700;
+  const maxHeight = 500;
 
-    const imageWidth = maxWidth / width;
-    const imageHeight = maxHeight / height;
+  const imageWidth = maxWidth / width;
+  const imageHeight = maxHeight / height;
 
-    const images = [];
-    for (let i = 0; i < width * height; i++) {
-      images.push(
-        <>
-          <img
-            key={i}
-            src={land}
-            alt={`Image ${i + 1}`}
-            style={{ width: `${imageWidth}px`, height: `${imageHeight}px`, border: '2px solid black'}}
-          />
-        </>
-      );
-    }
+  const images = []
+  const horizontal = []
+  const vertical = []
+  for (let i = 0; i < width * height; i++) {
+    images.push(
+      <img
+        key={i}
+        src={land}
+        alt={`Image ${i + 1}`}
+        style={{ width: `${imageWidth}px`, height: `${imageHeight}px`, border: '2px solid black'}}
+      />
+    );
+  }
 
-    return (
+  for (let i = 0; i < width; i++) {
+    vertical.push(
+      <p className='liHorizontal'>{i}</p>
+    );
+  }
+
+  for (let i = 0; i < height; i++) {
+    horizontal.push(
+      <p className='liVertical'>{i}</p>
+    );
+  }
+
+  return (
+    <div>
+      <div className='xVertical'>
+        {horizontal}
+      </div>
+      <div className='yHorizontal'>
+        {vertical}
+      </div>
       <div className='Board'>
         <div style={{ display: 'grid', gridTemplateColumns: `repeat(${width}, 1fr)`}}>
           {images}
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 interface TitleBoardProps {
@@ -97,8 +152,12 @@ function Body() {
         onRangeChange={handleRangeChange}
       />
       <Board
-        width={rangeValueX*4 || 1}
-        height={rangeValueY*4 || 1}
+        width={rangeValueX * 4 || 1}
+        height={rangeValueY * 4 || 1}
+      />
+      <ComandLine
+        width={rangeValueX * 4 || 1}
+        height={rangeValueY * 4 || 1}
       />
     </div>
   )
